6e04210b6ae3def27303aa99a9c94ce4e5acf175
Daniel Spiewak <djspiewak@gmail.com>
2011-02-13 20:18:55 -0600

    Namespace should be Optional

--- src/main/scala/com/codecommit/antixml/node.scala
+++ src/main/scala/com/codecommit/antixml/node.scala
@@ -4,7 +4,7 @@ sealed trait Node
 
 case class ProcInstr(target: String, data: String) extends Node
 
-case class Elem(namespace: String, name: String, attrs: Map[String, String], children: NodeSeq) extends Node
+case class Elem(ns: Option[String], name: String, attrs: Map[String, String], children: NodeSeq) extends Node
 
 case class Text(text: String) extends Node {
   override def toString = text
