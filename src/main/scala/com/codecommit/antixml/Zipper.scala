923c5a6cfd5bd4e386b34cc9a1cbeda4e00ae1cd
Daniel Spiewak <djspiewak@gmail.com>
2011-02-22 19:19:40 -0600

    Implemented Zipper#up method

--- src/main/scala/com/codecommit/antixml/Zipper.scala
+++ src/main/scala/com/codecommit/antixml/Zipper.scala
@@ -6,13 +6,25 @@ trait Zipper[+A <: Node] extends Group[A] { self =>
   val map: Vector[(Int, Int, Group[Node] => Node)]
   def parent: Zipper[Node]
   
-  val rebuild: Group[Node] => Group[Node] = null
-  val path: List[Group[Node] => Group[Node]] = null
-  
   // TODO this *may* be a poor choice of words...
   def stripZipper = new Group(toVector)
   
-  def up: Zipper[Node] = error("unimplemented")
+  def up: Zipper[Node] = {
+    val (_, nodes2) = map.foldLeft((0, parent.toVector)) {
+      case ((i, vec), (from, to, rebuild)) => {
+        val vec2 = vec(i) match {
+          case _: Elem => vec.updated(i, rebuild(self.slice(from, to)))
+          case _ => vec
+        }
+        (i + 1, vec2)
+      }
+    }
+    
+    new Group(nodes2) with Zipper[Node] {
+      val map = self.parent.map
+      def parent = self.parent.parent
+    }
+  }
   
   override def updated[B >: A <: Node](index: Int, node: B) = {
     new Group(super.updated(index, node).toVector) with Zipper[B] {
