2d87edca640bbd2054853920cf6b4c1451721298
David LaPalomento <dlapalomento@gmail.com>
2011-03-06 13:14:17 -0500

    cleaned up performance test framework. fixed up test-perf output a bit

--- src/test/scala/com/codecommit/antixml/Performance.scala
+++ src/test/scala/com/codecommit/antixml/Performance.scala
@@ -5,71 +5,86 @@ import com.github.dmlap.sizeof.SizeOf.deepsize
 import javax.xml.parsers.DocumentBuilderFactory 
 
 object Performance {
-  val tests: List[Test] = List(loadAntiSpending, loadScalaSpending, loadJavaSpending)
-  val comparisons: List[(Test, Test)] = List(loadAntiSpending -> loadScalaSpending)
-
   def main(args: Array[String]) {
-    println("Starting with " + (Runtime.getRuntime.maxMemory / (1024 * 1024)) + "MB")
+    println("-- System Information --")
+    println("Heap: " + (Runtime.getRuntime.maxMemory / (1024 * 1024)) + "MB")
+    println("Java: " + System.getProperty("java.vendor") + " " + System.getProperty("java.version"))
+    println("OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + " " + System.getProperty("os.arch"))
     println("-- Memory Usage --")
-    val memStart = System.nanoTime
-    println("anti-xml: " + deepsize(antiSpending))
-    println("scala.xml: " + deepsize(scalaSpending))
-    println("javax.xml: " + deepsize(javaSpending))
-    println("Memory Usage finished in " + durationMs(memStart) + " ms")
+    println("anti-xml: " + deepsize(LoadingXml.antiXml.run()))
+    println("scala.xml: " + deepsize(LoadingXml.scalaXml.run()))
+    println("javax.xml: " + deepsize(LoadingXml.javaXml.run()))
     println("-- Execution Time --")
-    val execStart = System.nanoTime
-    runTests()
-    println()
-    println("Execution Time finished in " + durationMs(execStart) + " ms")
-    println("-- Comparisons --")
-    val compStart = System.nanoTime
-    runComparisons()
-    println("Comparisons finished in " + durationMs(compStart) + " ms")
+    timeTrial(LoadingXml)
   }
 
-  @inline def durationMs(start: Long): Long =
+  def timedMs(f: =>Unit): Long = {
+    val start = System.nanoTime
+    f
     (System.nanoTime - start) / 1000000
+  }
 
-  def runTests() {
-    tests foreach { test =>
-      println(test.description)
-      println(runTest(test))
+  def timeTrial(trial: Trial) {
+    println(trial.description)
+    trial.implementations foreach { impl =>
+      print(" + " + impl.description + ": ")
+      (0 until trial.warmUps) foreach { _ => impl.run() }
+      println(TimingResults((0 until trial.runs) map { _ =>
+        System.gc()
+        timedMs { impl.run() }
+      }))
     }
   }
 
-  def runComparisons() {
-    comparisons foreach { case (left, right) =>
-      println(left.description + " versus " + right.description)
-      println(runTest(left) + "\t" + runTest(right))
-    }
-  }
+  case class Trial(val description: String) {
+    var implementations: Seq[Implementation] = Seq()
+    val warmUps = 5
+    val runs = 10
 
-  def runTest(test: Test): TimingResults = {
-    // warm-up
-    (0 until 5) foreach { _ => test.run() }
-    TimingResults((0 until 10) map { _ =>
-      System.gc()
-      val start = System.nanoTime
-      test.run()
-      durationMs(start)
-    })
-  }
-
-  trait Test {
-    val description: String
-    def run()
-  }
-  object Test {
-    def apply(desc: String)(f: () => Unit) = new Test {
-      val description = desc
-      def run() {
-        f()
+    object implemented {
+      def by(desc: String) = new {
+        def in(impl: =>Object): Implementation = {
+          val result = new Implementation  {
+            val description = desc
+            def run() = {
+              impl
+            }
+          }
+          implementations = implementations :+ result
+          result
+        }
       }
     }
+    
+    trait Implementation {
+      val description: String
+      def run(): Object
+    }
   }
 
-  class TimingResults private(val data: Seq[Long], val min: Long, val max: Long, val average: Long) {
-    override def toString = "min: " + min + " ms, max: " + max + " ms, average: " + average + " ms"
+  object LoadingXml extends Trial("Loading a 7MB XML file") {
+    val spendingPath = "/spending.xml"
+    val antiXml = implemented by "anti-xml" in {
+      XML.fromInputStream(getClass.getResourceAsStream(spendingPath))
+    }
+    val scalaXml = implemented by "scala.xml" in {
+      scala.xml.XML.load(getClass.getResourceAsStream(spendingPath))
+    }
+    val javaXml = implemented by "javax.xml" in {
+      DocumentBuilderFactory.newInstance.newDocumentBuilder.parse(getClass.getResourceAsStream(spendingPath))
+    }
+  }
+
+  // def loadDiscogs = Test("anti-xml loading a large file") { () =>
+  //   XML.fromInputStream(getClass.getResourceAsStream("/discogs_20110201_labels.xml"))
+  // }
+
+  class TimingResults private(val data: Seq[Long],
+                              val min: Long,
+                              val max: Long,
+                              val average: Long) {
+    override def toString = "min: " + min + " ms, max: " + max +
+    " ms, average: " + average + " ms"
   }
   object TimingResults {
     def apply(results: Seq[Long]): TimingResults = {
@@ -79,26 +94,4 @@ object Performance {
       new TimingResults(results, min, max, sum / results.size)
     }
   }
-
-  def loadDiscogs = Test("anti-xml loading a large file") { () =>
-    XML.fromInputStream(getClass.getResourceAsStream("/discogs_20110201_labels.xml"))
-  }
-
-  val spendingPath = "/spending.xml"
-
-  def antiSpending =
-    XML.fromInputStream(getClass.getResourceAsStream(spendingPath))
-  def loadAntiSpending =
-    Test("anti-xml loading a medium-sized XML file")(antiSpending _)
-
-  def scalaSpending =
-    scala.xml.XML.load(getClass.getResourceAsStream(spendingPath))
-  def loadScalaSpending =
-    Test("scala.xml loading a medium-sized XML file")(scalaSpending _)
-
-  def javaSpending =
-    DocumentBuilderFactory.newInstance.newDocumentBuilder.parse(getClass.getResourceAsStream(spendingPath))
-  def loadJavaSpending =
-    Test("javax.xml loading a medium-sized XML file")(javaSpending _)
-    
 }
