38cab7b0246c30986df0922207d46c16cc015ee3
Daniel Spiewak <djspiewak@gmail.com>
2011-03-09 20:53:42 -0600

    Added shallow- and deep-selection performance tests

--- src/test/scala/com/codecommit/antixml/Performance.scala
+++ src/test/scala/com/codecommit/antixml/Performance.scala
@@ -15,7 +15,8 @@ object Performance {
     println("scala.xml: " + deepsize(LoadingXml.scalaXml.run()))
     println("javax.xml: " + deepsize(LoadingXml.javaXml.run()))
     println("-- Execution Time --")
-    timeTrial(LoadingXml)
+    
+    List(LoadingXml, ShallowSelection, DeepSelection) foreach timeTrial
   }
 
   def timedMs(f: =>Unit): Long = {
@@ -43,7 +44,7 @@ object Performance {
 
     object implemented {
       def by(desc: String) = new {
-        def in(impl: =>Object): Implementation = {
+        def in(impl: =>Any): Implementation = {
           val result = new Implementation  {
             val description = desc
             def run() = {
@@ -58,7 +59,7 @@ object Performance {
     
     trait Implementation {
       val description: String
-      def run(): Object
+      def run(): Any
     }
   }
 
@@ -72,6 +73,86 @@ object Performance {
     }
     val javaXml = implemented by "javax.xml" in {
       DocumentBuilderFactory.newInstance.newDocumentBuilder.parse(getClass.getResourceAsStream(spendingPath))
+    }
+  }
+
+  object ShallowSelection extends Trial("Shallow selection in a 7MB tree") {
+    val spendingPath = "/spending.xml"
+    
+    val antiTree = XML.fromInputStream(getClass.getResourceAsStream(spendingPath))
+    val scalaTree = scala.xml.XML.load(getClass.getResourceAsStream(spendingPath))
+    val domTree = DocumentBuilderFactory.newInstance.newDocumentBuilder.parse(getClass.getResourceAsStream(spendingPath))
+    
+    val antiXml = implemented by "anti-xml" in {
+      antiTree \ "result" \ "doc" \ "MajorFundingAgency2"
+      antiTree \ "foo" \ "bar" \ "MajorFundingAgency2"
+    }
+    val scalaXml = implemented by "scala.xml" in {
+      scalaTree \ "result" \ "doc" \ "MajorFundingAgency2"
+      scalaTree \ "foo" \ "bar" \ "MajorFundingAgency2"
+    }
+    val javaXml = implemented by "javax.xml" in {
+      val childNodes = domTree.getChildNodes item 0 getChildNodes
+      
+      for {
+        i <- 0 until childNodes.getLength
+        val node = childNodes item i
+        if node.getNodeName == "result"
+        
+        val subChildren = node.getChildNodes
+        j <- 0 until subChildren.getLength
+        val subNode = subChildren item j
+        if subNode.getNodeName == "doc"
+        
+        val subChildren2 = node.getChildNodes
+        j <- 0 until subChildren2.getLength
+        val subNode2 = subChildren2 item j
+        if subNode2.getNodeName == "MajorFundingAgency2"
+      } yield subNode
+      
+      for {
+        i <- 0 until childNodes.getLength
+        val node = childNodes item i
+        if node.getNodeName == "foo"
+        
+        val subChildren = node.getChildNodes
+        j <- 0 until subChildren.getLength
+        val subNode = subChildren item j
+        if subNode.getNodeName == "bar"
+        
+        val subChildren2 = node.getChildNodes
+        j <- 0 until subChildren2.getLength
+        val subNode2 = subChildren2 item j
+        if subNode2.getNodeName == "MajorFundingAgency2"
+      } yield subNode
+    }
+  }
+
+  object DeepSelection extends Trial("Deep selection in a 7MB tree") {
+    val spendingPath = "/spending.xml"
+    
+    val antiTree = XML.fromInputStream(getClass.getResourceAsStream(spendingPath))
+    val scalaTree = scala.xml.XML.load(getClass.getResourceAsStream(spendingPath))
+    val domTree = DocumentBuilderFactory.newInstance.newDocumentBuilder.parse(getClass.getResourceAsStream(spendingPath))
+    
+    val antiXml = implemented by "anti-xml" in {
+      antiTree \\ "MajorFundingAgency2"
+      antiTree \\ "fubar"
+    }
+    val scalaXml = implemented by "scala.xml" in {
+      scalaTree \\ "MajorFundingAgency2"
+      scalaTree \\ "fubar"
+    }
+    val javaXml = implemented by "javax.xml" in {
+      {
+        val result = domTree getElementsByTagName "MajorFundingAgency2"
+        (0 until result.getLength) foreach (result item)
+      }
+      
+      {
+        val result = domTree getElementsByTagName "fubar"
+        (0 until result.getLength) foreach (result item)
+      }
     }
   }
 
