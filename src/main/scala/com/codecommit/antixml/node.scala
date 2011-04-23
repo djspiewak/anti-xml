d9d1779796d02338ca9150cb1eaec844fdca6165
Daniel Spiewak <djspiewak@gmail.com>
2011-04-22 22:22:57 -0500

    Added scaladoc for the Node ADT

--- src/main/scala/com/codecommit/antixml/node.scala
+++ src/main/scala/com/codecommit/antixml/node.scala
@@ -1,11 +1,65 @@
 package com.codecommit.antixml
 
+/**
+ * Root of the `Node` ADT, representing the different types of supported XML
+ * nodes which may appear in an XML fragment.  The ADT itself has the following
+ * shape (Haskell syntax):
+ *
+ * {{{
+ * data Node = ProcInstr String String
+ *           | Elem (Maybe String) String (Map String String) (Group Node)
+ *           | Text String
+ *           | EntityRef String
+ * }}}
+ *
+ * For those that don't find Haskell to be the clearest explanation of what's
+ * going on in this type, here is a more natural-language version.  The `Node`
+ * trait is sealed and has exactly four subclasses, each implementing a different
+ * type of XML node.  These four classes are as follows:
+ *
+ * <ul>
+ * <li>[[com.codecommit.antixml.ProcInstr]] – A processing instruction consisting
+ * of a target and some data</li>
+ * <li>[[com.codecommit.antixml.Elem]] – An XML element consisting of an optional
+ * namespace, a name (or identifier), a set of attributes and a sequence of child nodes</li>
+ * <li>[[com.codecommit.antixml.Text]] – A node containing a single string, representing
+ * character data in the XML tree</li>
+ * <li>[[com.codecommit.antixml.EntityRef]] – An entity reference (e.g. `&amp;`)</li>
+ * </ul>
+ */
 sealed trait Node
 
+/**
+ * A processing instruction consisting of a `target` and some `data`.  For example:
+ *
+ * {{{
+ * <?xml version="1.0"?>
+ * }}}
+ * 
+ * This would result in the following node:
+ *
+ * {{{
+ * ProcInstr("xml", "version=\"1.0\"")
+ * }}}
+ */
 case class ProcInstr(target: String, data: String) extends Node {
   override def toString = "<?" + target + " " + data + "?>"
 }
 
+/**
+ * An XML element consisting of an optional namespace, a name (or identifier), a
+ * set of attributes and a sequence of child nodes. For example:
+ *
+ * {{{
+ * <span id="foo" class="bar">Lorem ipsum</span>
+ * }}}
+ * 
+ * This would result in the following node:
+ *
+ * {{{
+ * Elem(None, "span", Map("id" -> "foo", "class" -> "bar"), Group(Text("Lorem ipsum")))
+ * }}}
+ */
 case class Elem(ns: Option[String], name: String, attrs: Map[String, String], children: Group[Node]) extends Node {
   override def toString = {
     val prefix = ns map { _ + ':' } getOrElse ""
@@ -24,10 +78,37 @@ case class Elem(ns: Option[String], name: String, attrs: Map[String, String], ch
   }
 }
 
+/**
+ * A node containing a single string, representing character data in the XML tree.
+ * For example:
+ *
+ * {{{
+ * Lorem ipsum dolor sit amet
+ * }}}
+ * 
+ * This would result in the following node:
+ *
+ * {{{
+ * Text("Lorem ipsum dolor sit amet")
+ * }}}
+ */
 case class Text(text: String) extends Node {
   override def toString = text
 }
 
+/**
+ * A node representing an entity reference. For example:
+ *
+ * {{{
+ * &hellip;
+ * }}}
+ * 
+ * This would result in the following node:
+ *
+ * {{{
+ * EntityRef("hellip")
+ * }}}
+ */
 case class EntityRef(entity: String) extends Node {
   override def toString = "&" + entity + ";"
 }
