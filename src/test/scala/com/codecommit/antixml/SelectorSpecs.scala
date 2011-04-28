07a7de9e2836d24b1a14b6a7817a476f3e864133
Daniel Spiewak <djspiewak@gmail.com>
2011-04-27 23:14:20 -0400

    Added some much needed specs for element selectors

--- src/test/scala/com/codecommit/antixml/SelectorSpecs.scala
+++ src/test/scala/com/codecommit/antixml/SelectorSpecs.scala
@@ -3,7 +3,7 @@ package com.codecommit.antixml
 import org.specs._
 
 object SelectorSpecs extends Specification {
-  "the * selector should" in {
+  "the * selector" should {
     "select nothing when parent is empty" in {
       XML.fromString("<parent/>") \ * mustEqual Group()
     }
@@ -12,6 +12,18 @@ object SelectorSpecs extends Specification {
       val xml = XML.fromString("<parent><child1/>Test<child2/>text here we go \n with whitespace<child3>Inside!</child3></parent>")
       val expected = xml.children
       xml \ * mustEqual expected
+    }
+  }
+  
+  "the element selector(s)" should {
+    "select nothing when parent is empty" in {
+      (<parent/>).anti \ "parent" mustEqual Group()
+      (<parent/>).anti \ 'parent mustEqual Group()
+    }
+    
+    "select only the named element(s)" in {
+      (<parent><foo/><bar/>Baz<foo/></parent>).anti \ "foo" mustEqual Group(<foo/>.anti, <foo/>.anti)
+      (<parent><foo/><bar/>Baz<foo/></parent>).anti \ 'foo mustEqual Group(<foo/>.anti, <foo/>.anti)
     }
   }
 }
