206cb38dd1de950837f863793c141af02335a3e3
Daniel Spiewak <djspiewak@gmail.com>
2011-05-06 18:36:59 -0500

    Added the ability to configure the options passed to Java by the standalone build script

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@
 
 SUM=`openssl sha1 /tmp/sbt-launch-0.7.5.jar | sed 's!SHA1(/tmp/sbt-launch-0.7.5.jar)= !!g'`
 
+[ -n "$JAVA_OPTS" ] || JAVA_OPTS="-Xmx1024m"
+
 if [ "${SUM}" == "b398555a69ef9317f7840020fa57bd8f23eaf60e" ]; then
-	exec java -Xmx1024m -jar /tmp/sbt-launch-0.7.5.jar $*
+	exec java $JAVA_OPTS -jar /tmp/sbt-launch-0.7.5.jar $*
 else
 	echo 'Downloaded SBT launcher 0.7.5 but did not match the checksum on record!'
 	exit -1
