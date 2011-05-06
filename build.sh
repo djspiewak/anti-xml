c95b26e0535e1148b0e36903669c1b58fb8df3ef
Daniel Spiewak <djspiewak@gmail.com>
2011-05-06 15:29:11 -0500

    Generalized build.sh somewhat

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 SUM=`openssl sha1 /tmp/sbt-launch-0.7.5.jar | sed 's!SHA1(/tmp/sbt-launch-0.7.5.jar)= !!g'`
 
 if [ "${SUM}" == "b398555a69ef9317f7840020fa57bd8f23eaf60e" ]; then
-	exec java -Xmx1024m -jar /tmp/sbt-launch-0.7.5.jar update test-compile package doc
+	exec java -Xmx1024m -jar /tmp/sbt-launch-0.7.5.jar $*
 else
 	echo 'Downloaded SBT launcher 0.7.5 but did not match the checksum on record!'
 	exit -1
