a092c84bd97d79b7d034acdf38195e9670cf7fac
Daniel Spiewak <djspiewak@gmail.com>
2011-05-06 21:53:05 -0500

    Stubbed LazyVector prototype

---
+++ src/main/scala/com/codecommit/antixml/util/LazyVector.scala
@@ -0,0 +1,25 @@
+package com.codecommit.antixml.util
+
+private[antixml] class LazyVector[S, +A] private (body: Vector[A], tail: Vector[A], state: S, f: S => Option[(S, A)]) {
+  def apply(i: Int): (A, LazyVector[S, A]) = null
+  
+  def updated[B >: A](i: Int, b: B): LazyVector[S, B] = null
+  
+  def +:[B >: A](b: B): LazyVector[S, B] = null
+  
+  def :+[B >: A](b: B): LazyVector[S, B] = null
+  
+  def ++[B >: A](that: LazyVector[S, B]): LazyVector[S, B] = null
+  
+  def map[B](f: A => B): LazyVector[S, B] = null
+  
+  def flatMap[B](f: A => LazyVector[S, B]): LazyVector[S, B] = null
+  
+  def collect[B](pf: PartialFunction[A, B]): LazyVector[S, B] = null
+  
+  def force: Vector[A] = null
+}
+
+private[antixml] object LazyVector {
+  def apply[S, A](init: S)(f: S => (S, A)): LazyVector[S, A] = null
+}
